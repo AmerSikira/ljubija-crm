3124944400e246e6e9edcdcca28d7293fd9b0209
Amer Sikira <amersikira@Amers-MacBook-Air.local>
2026-01-21 12:10:44 +0100

    Changed logo

--- resources/js/components/app-logo.tsx
+++ resources/js/components/app-logo.tsx
@@ -3,7 +3,7 @@ import AppLogoIcon from './app-logo-icon';
 export default function AppLogo() {
     return (
         <>
-            <img src='/logo-delba.png'/>
+            <img src='/logo-delba-dzemat.png' />
         </>
     );
 }
