e19600fbf97ebb49f58443336e4d653b045c9c9d
Amer Sikira <amersikira@Amers-MacBook-Air.local>
2026-02-19 21:45:54 +0100

    Fixed footer color issues

--- resources/js/components/content-holder.tsx
+++ resources/js/components/content-holder.tsx
@@ -10,7 +10,7 @@ export default function ContentHolder({ children, className }: ContentHolderProp
     <div className={`w-full px-8 py-8 min-h-screen ${className ?? ''}`}>
       {children}
 
-      <div className="text-center mt-8">
+      <div className="text-center mt-8 dark:text-white">
         Made by <a href="https://axiom.ba" className="text-green-600">Axiom doo</a> | Powered by <a href="https://prosilva.eu" className="text-green-600">ProSilva doo</a>
       </div>
     </div>
